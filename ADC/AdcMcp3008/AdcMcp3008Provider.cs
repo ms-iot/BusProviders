685f9c46c382bf5f6213bac2c4a388beffc1865d
David Shoemaker <David.Shoemaker@microsoft.com>
2015-10-19 14:07:29 -0700

    Addition of a MCP3008 ADC Provider

---
+++ ADC/AdcMcp3008/AdcMcp3008Provider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Windows.Devices.Adc.Provider;
+
+namespace AdcMcp3008
+{
+    public sealed class AdcMcp3008Provider : IAdcProvider
+    {
+        AdcMcp3008Provider()
+        {
+        }
+
+        static IAdcProvider providerSingleton = null;
+
+        static public IAdcProvider GetAdcProvider()
+        {
+            if (providerSingleton == null)
+            {
+                providerSingleton = new AdcMcp3008Provider();
+            }
+            return providerSingleton;
+
+        }
+
+        public IReadOnlyList<IAdcControllerProvider> GetControllers()
+        {
+            AdcMcp3008ControllerProvider provider = new AdcMcp3008ControllerProvider();
+
+            List<IAdcControllerProvider> list = new List<IAdcControllerProvider>();
+            list.Add(provider);
+
+            return list;
+        }
+    }
+}
